5829aae8c366104fce214eade5c7c9bd17868018
VictoriaBubbleGun <jennyfer.mueller@hotmail.com>
2023-07-02 22:09:30 +0200

    JS realtime included
    done the Js so that its displaying real time

--- javascript/index.js
+++ javascript/index.js
@@ -1,27 +1,52 @@
-function timezone() {
-  if (event.target.value.length > 0) {
-    if (event.target.value === "berlin") {
-      alert(
-        moment
-          .tz("Europe/Berlin")
-          .format("[It is ]dddd, MMMM Do YYYY hh:mm a[ in Berlin]")
-      );
-    }
-    if (event.target.value === "paris") {
-      alert(
-        moment
-          .tz("Europe/Paris")
-          .format("[It is ]dddd, MMMM Do YYYY hh:mm a[ in Paris]")
-      );
-    }
-    if (event.target.value === "jamaica") {
-      alert(
-        moment
-          .tz("America/Jamaica")
-          .format("[It is ]dddd, MMMM Do YYYY hh:mm a[ in Jamaica]")
-      );
-    }
-  }
+//berlin
+
+function clock() {
+  let berlin = document.querySelector("#berlin");
+  let berlinDate = berlin.querySelector(".date");
+  berlinDate.innerHTML = moment
+    .tz("Europe/Berlin")
+    .format("dddd, MMMM Do YYYY");
+  let berlinTime = berlin.querySelector(".time");
+  berlinTime.innerHTML = moment
+    .tz("Europe/Berlin")
+    .format("hh:mm:ss[<small>]a[</small>]");
 }
-let countrySelector = document.querySelector("#country");
-countrySelector.addEventListener("change", timezone);
+berlinTime = setInterval(clock, 1000);
+
+//paris
+function clock1() {
+  let paris = document.querySelector("#paris");
+  let parisDate = paris.querySelector(".date");
+  parisDate.innerHTML = moment.tz("Europe/Paris").format("dddd, MMMM Do YYYY");
+  let parisTime = paris.querySelector(".time");
+  parisTime.innerHTML = moment
+    .tz("Europe/Paris")
+    .format("hh:mm:ss[<small>]a[</small>]");
+}
+parisTime = setInterval(clock1, 1000);
+
+//hong-kong
+function clock2() {
+  let hongKong = document.querySelector("#hong-kong");
+  let hongKongDate = hongKong.querySelector(".date");
+  hongKongDate.innerHTML = moment
+    .tz("Asia/Hong_Kong")
+    .format("dddd, MMMM Do YYYY");
+  let hongKongTime = hongKong.querySelector(".time");
+  hongKongTime.innerHTML = moment
+    .tz("Asia/Hong_Kong")
+    .format("hh:mm:ss[<small>]a[</small>]");
+}
+hongKongTime = setInterval(clock2, 1000);
+
+//hawaii
+function clock3() {
+  let hawaii = document.querySelector("#hawaii");
+  let hawaiiDate = hawaii.querySelector(".date");
+  hawaiiDate.innerHTML = moment.tz("Us/Hawaii").format("dddd, MMMM Do YYYY");
+  let hawaiiTime = hawaii.querySelector(".time");
+  hawaiiTime.innerHTML = moment
+    .tz("US/Hawaii")
+    .format("hh:mm:ss[<small>]a[</small>]");
+}
+hawaiiTime = setInterval(clock3, 1000);
